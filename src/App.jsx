bf0f6b58ac920b04aa1dd8427144be0284f9cf96
Olesia Shcherbenko <oshch96@gmail.com>
2025-05-17 18:32:52 +0200

    feat: add my first custom component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  );
+}
